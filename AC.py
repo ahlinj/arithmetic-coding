2c49859cae2d410c0a009a76f65c63eeb403da06
Jaka <jakaahlin0@gmail.com>
2025-05-07 12:02:53 +0200

    generating a sequence

--- AC.py
+++ AC.py
@@ -4,8 +4,11 @@ Created on Wed May  7 11:13:12 2025
 
 @author: Jaka
 """
+import random
 
 SEQ_LEN = 100
 alphabet = ['A','B','C','D']
 prob_dist = [0.5,0.3,0.1,0.1]
 
+sequence = random.choices(alphabet, weights=prob_dist, k=SEQ_LEN)
+print(''.join(sequence))
